9a64d872890d905208dea55d45e28ed31d86ec72
dim web <dmitrymail_k@mail.ru>
2021-07-14 14:35:46 +0300

    add debug folder

--- learn_open_gl/projects/00__first_project/first_project/first_project/test.cpp
+++ learn_open_gl/projects/00__first_project/first_project/first_project/test.cpp
@@ -2,8 +2,12 @@
 #include <GLFW\glfw3.h>
 #include <iostream>
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+
 int main()
-{
+{ 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -18,6 +22,42 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	// set callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	
+	// простой render loop
+	// мы не хотим чтобы приложение сразу закрылось, поэтому выставляем некоторое условие
+	while (!glfwWindowShouldClose(window))
+	{
+		/*
+		механизм отрисовки окна double buffer.
+		Почему double buffer, потому что если бы у нас был один массив, который бы отрисовывал
+		и изменял единый buffer, это повлекло бы за собой фризы на экране. 
+		Поэтому есть 2 buffer, один который отрисовывается, другой который обрабатывается
+		и готовится к отрисовке.
+		*/
+		glfwSwapBuffers(window);
+		
+		// проверяет все events с клавиатуры и экрана
+		glfwPollEvents();
+	}
+	// чистит кеш и корректно завершает работу с glfw
+	glfwTerminate();
+
+
+
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{	
+	// callback for resizing window
+	glViewport(0, 0, width, height);
 }
