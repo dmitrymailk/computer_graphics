3dff95a8f233680870a174565875fc15780450f1
dim web <dmitrymail_k@mail.ru>
2021-07-14 14:02:59 +0300

    small change

--- learn_open_gl/projects/00__first_project/first_project/first_project/test.cpp
+++ learn_open_gl/projects/00__first_project/first_project/first_project/test.cpp
@@ -8,7 +8,6 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
