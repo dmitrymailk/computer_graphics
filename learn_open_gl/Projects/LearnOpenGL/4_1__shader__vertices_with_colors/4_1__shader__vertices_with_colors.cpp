a6ee0d7986448fda328853464dc2deeb9c9978de
dim web <dmitrymail_k@mail.ru>
2021-10-06 21:10:48 +0300

    read shades from file

--- learn_open_gl/Projects/LearnOpenGL/4_1__shader__vertices_with_colors/4_1__shader__vertices_with_colors.cpp
+++ learn_open_gl/Projects/LearnOpenGL/4_1__shader__vertices_with_colors/4_1__shader__vertices_with_colors.cpp
@@ -15,17 +15,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = ourColor;\n"
+"   FragColor = vec4(ourColor, 1.0);\n"
 "}\n\0";
 
 int main()
@@ -100,9 +103,11 @@ int main()
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	float vertices[] = {
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		 0.0f,  0.5f, 0.0f   // top 
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+
 	};
 
 	unsigned int VBO, VAO;
@@ -114,8 +119,18 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(
+		0, // attach to ID=1 VAO
+		3, // size of numbers
+		GL_FLOAT, // data type of array
+		GL_FALSE, // we don't want
+		6 * sizeof(float), // stride
+		(void*)0); // stride start
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
@@ -143,15 +158,9 @@ int main()
 		glUseProgram(shaderProgram);
 
 		// NEW CONTENT
-		// update shader uniform
-		float timeValue = glfwGetTime();
-		float greenValue = sin(timeValue) / 1.0f + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-		// NEW CONTENT
-
 		// render the triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// NEW CONTENT
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
