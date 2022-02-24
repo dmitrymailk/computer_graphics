c1d18a776d4f4ac2c57fd38256cc540a6bb00570
dim web <dmitrymail_k@mail.ru>
2022-02-24 18:54:44 +0300

    initial vue 3 setup

---
+++ web-computer-graphics/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Homepage test</h1>
+  </div>
+</template>
+
+<style lang="sass">
+.container
+	width: 100%
+	text-align: center
+	background: #ccc
+</style>
