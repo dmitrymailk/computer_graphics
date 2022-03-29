d2c4233112772248644fb54544df639e3b48b0b6
dmitrymailk <dmitrymail_k@mail.ru>
2022-03-29 22:26:17 +0300

    b-spline implementation

--- web-computer-graphics/src/components/Lab3/bspline.ts
+++ web-computer-graphics/src/components/Lab3/bspline.ts
@@ -1,13 +1,13 @@
 function interpolate(
   t: number,
   degree: number,
-  points: Array<Array<number>>,
+  controlPoints: Array<Array<number>>,
   knots: Array<number>,
   weights: Array<number>
 ) {
-  var i, j, s, l; // function-scoped iteration variables
-  var n = points.length; // points count
-  var d = points[0].length; // point dimensionality
+  let i, j, startKnot, level; // function-scoped iteration variables
+  let n = controlPoints.length; // points count
+  let vectorDimension = controlPoints[0].length; // point dimensionality
 
   if (degree < 1) throw new Error("degree must be at least 1 (linear)");
   if (degree > n - 1)
@@ -32,50 +32,53 @@ function interpolate(
       throw new Error("bad knot vector length");
   }
 
-  var domain = [degree, knots.length - 1 - degree];
+  let domain = [degree, knots.length - 1 - degree];
 
   // remap t to the domain where the spline is defined
-  var low = knots[domain[0]];
-  var high = knots[domain[1]];
+  let low = knots[domain[0]];
+  let high = knots[domain[1]];
   t = t * (high - low) + low;
 
   if (t < low || t > high) throw new Error("out of bounds");
 
   // find s (the spline segment) for the [t] value provided
-  for (s = domain[0]; s < domain[1]; s++) {
-    if (t >= knots[s] && t <= knots[s + 1]) {
+  for (startKnot = domain[0]; startKnot < domain[1]; startKnot++) {
+    if (t >= knots[startKnot] && t <= knots[startKnot + 1]) {
       break;
     }
   }
+  console.log(startKnot);
 
   // convert points to homogeneous coordinates
-  var v = [];
+  // If we have NURBS https://en.wikipedia.org/wiki/Non-uniform_rational_B-spline
+  let v: Array<Array<number>> = [];
   for (i = 0; i < n; i++) {
     v[i] = [];
-    for (j = 0; j < d; j++) {
-      v[i][j] = points[i][j] * weights[i];
+    for (j = 0; j < vectorDimension; j++) {
+      v[i][j] = controlPoints[i][j] * weights[i];
     }
-    v[i][d] = weights[i];
+    v[i][vectorDimension] = weights[i];
   }
 
   // l (level) goes from 1 to the curve degree + 1
-  var alpha;
-  for (l = 1; l <= degree + 1; l++) {
-    // build level l of the pyramid
-    for (i = s; i > s - degree - 1 + l; i--) {
-      alpha = (t - knots[i]) / (knots[i + degree + 1 - l] - knots[i]);
+  let alpha;
+  for (level = 1; level <= degree + 1; level++) {
+    // строим пирамиду из N коэфициентов
+    for (i = startKnot; i > startKnot - degree - 1 + level; i--) {
+      alpha = (t - knots[i]) / (knots[i + degree + 1 - level] - knots[i]);
 
       // interpolate each component
-      for (j = 0; j < d + 1; j++) {
+      for (j = 0; j < vectorDimension + 1; j++) {
         v[i][j] = (1 - alpha) * v[i - 1][j] + alpha * v[i][j];
       }
     }
   }
+  // debugger;
 
   // convert back to cartesian and return
-  var result = result || [];
-  for (i = 0; i < d; i++) {
-    result[i] = v[s][i] / v[s][d];
+  let result = [];
+  for (i = 0; i < vectorDimension; i++) {
+    result[i] = v[startKnot][i] / v[startKnot][vectorDimension];
   }
 
   return result;
--- web-computer-graphics/src/components/Lab3/lab3_1.ts
+++ web-computer-graphics/src/components/Lab3/lab3_1.ts
@@ -48,9 +48,9 @@ class Lab_3_1 {
     this.clearScreen();
     this.setPoints();
     // this.drawB_Spline_old();
-    // if (this.coords_3_1.length > 2) this.drawB_Spline_3();
+    if (this.coords_3_1.length > 2) this.drawB_Spline_3();
     // if (this.coords_3_1.length > 2) this.drawB_Spline_4();
-    if (this.coords_3_1.length > 2) this.drawB_Spline_2();
+    // if (this.coords_3_1.length > 2) this.drawB_Spline_2();
   }
   private setPoints() {
     for (let coord of this.coords_3_1) {
@@ -59,73 +59,7 @@ class Lab_3_1 {
   }
 
   private debug_func() {
-    const degree = 2;
-    const len = 3 + degree + 1;
-    this.knots = [];
-    let knotNum = 0;
-    for (let i = 0; i < len; i++) {
-      // if (i < degree) this.knots.push(knotNum / len);
-      // else if (i < len - degree - 1) {
-      //   this.knots.push(knotNum / len);
-      //   knotNum += 1;
-      // } else {
-      //   this.knots.push(knotNum / len);
-      // }
-      this.knots.push(i / degree);
-    }
-    console.log(this.knots);
-    for (let u = 0.1; u < 1; u += 0.1) {
-      let sum = 0;
-      console.log("----------------------");
-      for (let i = 0; i < 3; i++) {
-        // const u = 0;
-        const res = this.N(i, degree, u);
-        console.log(`i=${i} deg=${degree} u=${u} N(i,deg,n) ${res}`);
-        sum += res;
-      }
-      console.log(`SUM=${sum}`);
-    }
-  }
-
-  drawB_Spline_4() {
-    const degree = 2;
-    const len = this.coords_3_1.length + degree + 1;
-    this.knots = [];
-    let knotNum = 0;
-    for (let i = 0; i < len; i++) {
-      // if (i < degree) this.knots.push(knotNum / len);
-      // else if (i < len - degree - 1) {
-      //   this.knots.push(knotNum / len);
-      //   knotNum += 1;
-      // } else {
-      //   this.knots.push(knotNum / len);
-      // }
-      this.knots.push(i / degree);
-    }
-    console.log(this.knots);
-    let points = [];
-    for (let u = 0.1; u < 1; u += 0.1) {
-      // let sum = 0;
-      let x = 0;
-      let y = 0;
-      console.log("----------------------");
-      for (let i = 0; i < 3; i++) {
-        // const u = 0;
-        const res = this.N(i, degree, u);
-        // console.log(`i=${i} deg=${degree} u=${u} N(i,deg,n) ${res}`);
-        // sum += res;
-        x = this.coords_3_1[i].x + (1 - res) * this.coords_3_1[i].x;
-        y = this.coords_3_1[i].y + (1 - res) * this.coords_3_1[i].y;
-        points.push([x, y]);
-      }
-      // console.log(`SUM=${sum}`);
-    }
-
-    for (let i = 0; i < points.length - 1; i++) {
-      const v1 = points[i];
-      const v2 = points[i + 1];
-      this.canvas.drawLine(v1[0], v1[1], v2[0], v2[1]);
-    }
+    this.drawB_Spline_3();
   }
 
   drawB_Spline_3() {
@@ -137,89 +71,62 @@ class Lab_3_1 {
     }
     const degree = 2;
     let knots = [];
-    const len = this.coords_3_1.length + degree + 1;
+    const n = this.coords_3_1.length;
+    const len = n + degree + 1;
     let knotNum = 0;
     for (let i = 0; i < len; i++) {
-      // if (i < degree) knots.push(knotNum / degree);
-      // else if (i < len - degree - 1) {
-      //   knots.push(knotNum / degree);
-      //   knotNum += 1;
-      // } else {
-      //   knots.push(knotNum / degree);
-      // }
-      knots.push(i / (len - 1));
+      if (i <= degree) knots.push(0);
+      else if (i < len - degree - 1) {
+        knotNum += 1;
+        knots.push(i / len);
+      } else {
+        knots.push(1);
+      }
+      // knots.push(i / (len - 1));
     }
+    console.log(knots);
+    // debugger;
 
     this.knots = knots;
-
-    // console.log(knots);
+    // // this.knots = [0, 0, 0, 0.5, 1, 1, 1];
+    // // console.log(knots);
     let pointsAmount = this.coords_3_1.length;
-    for (let u = precision; u < 1 - precision; u += precision) {
+    for (let u = 0; u <= 1; u += precision) {
       let x = 0;
       let y = 0;
       for (let i = 0; i < pointsAmount; i++) {
         // console.log(this.N(i, pointsAmount, u) * this.coords_3_1[i].x);
-        x += this.N(i, pointsAmount, u) * this.coords_3_1[i].x;
-        y += this.N(i, pointsAmount, u) * this.coords_3_1[i].y;
+        let res = this.N(i, degree, u);
+        x += res * this.coords_3_1[i].x;
+        y += res * this.coords_3_1[i].y;
       }
-
       points.push([x, y]);
-
       console.log([x, y]);
     }
-    // console.log(points);
+    // for (let u_i = 0; u_i < this.knots.length - 1; u_i++) {
+    //   for (let x_1 = this.knots[u_i]; x_1 < this.knots[u_i + 1]; x_1 += 0.001) {
+    //     let x = 0;
+    //     let y = 0;
+    //     for (let i = 0; i < pointsAmount; i++) {
+    //       // console.log(this.N(i, pointsAmount, u) * this.coords_3_1[i].x);
+    //       let res = this.N(i, degree, x_1);
+    //       x += res * this.coords_3_1[i].x;
+    //       y += res * this.coords_3_1[i].y;
+    //       // debugger;
+    //     }
+    //     if (Number.isNaN(x) || Number.isNaN(y)) {
+    //       debugger;
+    //     }
+    //     points.push([x, y]);
+    //     console.log([x, y]);
+    //   }
+    // }
 
     for (let i = 0; i < points.length - 1; i++) {
       const v1 = points[i];
       const v2 = points[i + 1];
       this.canvas.drawLine(v1[0], v1[1], v2[0], v2[1]);
     }
-  }
-
-  debour(
-    i: number,
-    u: number,
-    knots: Array<number>,
-    controlPoints: Array<Array<number>>,
-    degree: number,
-    pos: number
-  ) {
-    `
-    def deBoor(k: int, x: int, t, c, p: int):
-    """Evaluates S(x).
-
-    Arguments
-    ---------
-    k: Index of knot interval that contains x.
-    x: Position.
-    t: Array of knot positions, needs to be padded as described above.
-    c: Array of control points.
-    p: Degree of B-spline.
-    """
-    d = [c[j + k - p] for j in range(0, p + 1)]
-
-    for r in range(1, p + 1):
-        for j in range(p, r - 1, -1):
-            alpha = (x - t[j + k - p]) / (t[j + 1 + k - r] - t[j + k - p])
-            d[j] = (1.0 - alpha) * d[j - 1] + alpha * d[j]
-
-    return d[p]
-    `;
-    let d: Array<number> = [];
-    for (let j = 0; j < controlPoints.length; j++) {
-      // debugger;
-      d.push(controlPoints[j + i - degree][pos]);
-    }
-    for (let r = 1; r < degree + 1; r++) {
-      for (let j = r - 1; j > degree; j--) {
-        let alpha =
-          (u - knots[j + i - degree]) /
-          (knots[j + 1 + i - r] - knots[j + i - degree]);
-        d[j] = (1.0 - alpha) * d[j - 1] + alpha * d[j];
-      }
-    }
-
-    return d[degree];
   }
 
   drawB_Spline_2() {
@@ -234,18 +141,20 @@ class Lab_3_1 {
     const len = this.coords_3_1.length + degree + 1;
     let knotNum = 0;
     for (let i = 0; i < len; i++) {
-      if (i < degree) knots.push(knotNum);
-      else if (i < len - degree - 1) {
+      if (i < degree - 1) knots.push(knotNum);
+      else if (i < len - degree) {
         knots.push(knotNum / degree);
-        knotNum += 1;
+        knotNum = 1;
       } else {
         knots.push(knotNum / degree);
       }
     }
+    // debugger;
     // console.log(knots);
     for (let i = 0; i < 1; i += 1 / precision) {
       // @ts-ignore
       let point = interpolate(i, degree, controlPoints, knots);
+      console.log(point);
       points.push(point);
     }
 
@@ -261,75 +170,56 @@ class Lab_3_1 {
     // console.log("clear");
   }
 
-  drawB_Spline_old() {
-    let points_x: Array<number> = [];
-    let points_y: Array<number> = [];
-    let grid_N = 100;
+  // N(index: number, order: number, u: number): number {
+  //   let coef1: number, coef2: number;
+  //   if (order == 1) {
+  //     if (index == 0)
+  //       if (this.knots[index] <= u && u <= this.knots[index + 1]) return 1.0;
+  //     if (this.knots[index] < u && u <= this.knots[index + 1]) return 1.0;
+  //     else return 0.0;
+  //   }
+  //   if (this.knots[index + order - 1] == this.knots[index]) {
+  //     if (u == this.knots[index]) coef1 = 1;
+  //     else coef1 = 0;
+  //   } else
+  //     coef1 =
+  //       (u - this.knots[index]) /
+  //       (this.knots[index + order - 1] - this.knots[index]);
 
-    for (let item of this.coords_3_1) {
-      points_x.push(item.x);
-      points_y.push(item.y);
-    }
+  //   if (this.knots[index + order] == this.knots[index + 1]) {
+  //     if (u == this.knots[index + order]) coef2 = 1;
+  //     else coef2 = 0;
+  //   } else
+  //     coef2 =
+  //       (this.knots[index + order] - u) /
+  //       (this.knots[index + order] - this.knots[index + 1]);
 
-    let min_x = Math.min(...points_x);
-    let max_x = Math.max(...points_x);
-    let min_y = Math.min(...points_y);
-    let max_y = Math.max(...points_y);
-
-    let delta_grid_x: number = (max_x - min_x) / grid_N;
-    let delta_grid_y: number = (max_x - min_x) / grid_N;
-    let subgrid_N = 5;
-    let delta_x = delta_grid_x / subgrid_N;
-    let delta_y = delta_grid_y / subgrid_N;
-    this.grid_points_x = [];
-
-    for (let delta = min_x; delta < max_x; delta += delta_grid_x) {
-      this.grid_points_x.push(delta);
-    }
-    for (let delta = min_y; delta < max_y; delta += delta_grid_x) {
-      this.grid_points_x.push(delta);
-    }
-
-    for (let i = 0; i < this.grid_points_x.length - 1; i++) {
-      for (
-        let delta = this.grid_points_x[i];
-        delta < this.grid_points_x[i + 1];
-        delta += delta_x
-      ) {}
-    }
-    // for (let i = 0; i < this.grid_points_y.length - 1; i++) {
-    //   for (
-    //     let delta = this.grid_points_x[i];
-    //     delta < this.grid_points_x[i + 1];
-    //     delta += delta_x
-    //   ) {
-
-    //   }
-    // }
-  }
-
-  N(i: number, m: number, u: number) {
-    // debugger;
+  //   return (
+  //     coef1 * this.N(index, order - 1, u) +
+  //     coef2 * this.N(index + 1, order - 1, u)
+  //   );
+  // }
+  N(i: number, m: number, u: number): number {
     if (m == 0) {
-      if (this.knots[i] <= u && u <= this.knots[i + 1]) return 1.0;
-      return 0.0;
-    } else {
-      let part_1: number =
-        this.div(u - this.knots[i], this.knots[i + m] - this.knots[i]) *
-        this.N(i, m - 1, u);
-
-      let part_2: number =
-        this.div(
-          this.knots[i + m + 1] - u,
-          this.knots[i + m + 1] - this.knots[i + 1]
-        ) * this.N(i + 1, m - 1, u);
-
-      return part_1 + part_2;
+      if (this.knots[i] <= u && u <= this.knots[i + 1]) return 1;
+      return 0;
     }
-  }
-  div(num_1: number, num_2: number) {
-    // if (num_1 == 0 || num_2 == 0) return 0;
-    return num_1 / num_2;
+    let part_1: number;
+    if (this.knots[i + m] == this.knots[i]) {
+      if (u == this.knots[i]) part_1 = 0;
+      else part_1 = 1;
+    } else part_1 = (u - this.knots[i]) / (this.knots[i + m] - this.knots[i]);
+
+    let part_2: number;
+    if (this.knots[i + m + 1] == this.knots[i + 1]) {
+      if (this.knots[i + m + 1] == u) part_2 = 0;
+      else part_2 = 1;
+    } else
+      part_2 =
+        (this.knots[i + m + 1] - u) /
+        (this.knots[i + m + 1] - this.knots[i + 1]);
+
+    return part_1 * this.N(i, m - 1, u) + part_2 * this.N(i + 1, m - 1, u);
   }
 }
 
